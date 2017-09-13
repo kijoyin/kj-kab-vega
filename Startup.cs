2eb924a44703621b29616880b32bf7b6bc516bcc
Kiran Joy <kijoyin@outlook.com>
2017-09-13 13:16:09 +1000

    Day 3

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using vega.Persistence;
 
 namespace vega
 {
@@ -22,6 +24,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(opt => opt.)
             services.AddMvc();
         }
 
